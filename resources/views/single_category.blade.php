1e41c58e8c8389f9a662d91267232b904116cdfb
dinesh <dlaller1988@gmail.com>
2024-04-02 12:44:55 +0530

    changes in single page

--- resources/views/single_category.blade.php
+++ resources/views/single_category.blade.php
@@ -1,7 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1> Code here </h1>
-
+<div class="singlePageDetail">
+  <div class="wrapper">
+    <div class="singlePageDetailHub">
+      <div class="leftSideSlider">
+        <div class="sideCategoryInfo">
+          <h6>Universal Phone Stand Mirror Mount Navigation Accessories Vehicle.</h6>
+          <p><b>23</b> Views | <span>Canada</span></p>
+        </div>
+        <div class="categorySliding">
+          <div class="videos-slider-2">
+          <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+          </div>
+          <div class="slider-nav-thumbnails">
+          <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+              <div>
+                <img src="https://img.youtube.com/vi/1wFfavnPOVw/hqdefault.jpg" alt="" />
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="rightCategoryInfo">
+        <p>Info Ends in <b>3 Days</b></p>
+        <h6>Phone Stand Mirror Mount Navigation Accessories Vehicle</h6>
+        <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the
+          visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be
+          used as a placeholder before the final copy is available.</p>
+        <ul>
+          <li>Best for industry</li>
+          <li>Best for industry</li>
+          <li>Info as per category</li>
+        </ul>
+        <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the
+          visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be
+          used as a placeholder before the final copy is available.</p>
+        <div class="buttonsCategory">
+          <section><button>Contact US</button><button>Email</button></section>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
