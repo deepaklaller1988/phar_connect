cde1dc5e619d5800b05df8ef8f8e4a8e2fa9b8b8
dinesh <dlaller1988@gmail.com>
2024-04-04 18:12:34 +0530

    changes

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,40 +3,6 @@
 
 
 <div class="midContainer">
-<<<<<<< HEAD
-    <div class="bannerSet bannerSetcategory">
-        <div class="wrapper">
-            <h4>Welcome to XXX, where expertise meet opportunity.</h4>
-            <p>Our goal is to revolutionize how services and industries integral to all aspects of drug development and
-                commercialization
-                connect and collaborate with Biotechnology and Pharmaceutical companies.</p>
-            <p>Our platform facilitates seamless connections, powering a revolution in drug development and
-                commercialization by providing
-                a single global platform connecting diverse array of service providers and experts.</p>
-            <p>Join us as we shape the future of drug development and commercialization, one connection at a time.</p>
-        </div>
-    </div>
-    <div class="pharmCategory categoriesAll catgorySetsAll">
-        <div class="wrapper">
-            <div class="pharmCategoryInner">
-                <section class="categorySet">
-                    @foreach($data['categories'] as $category)
-                    <div>
-                        <a href="{{ route('subcategory',$category->id) }}">
-                            <span><img src="{{ url('storage/'.$category->image) }}" alt="categoriy" /></span>
-                            <h4>{{ $category->title}}</h4>
-                            <b>Learn More</b>
-                        </a>
-                    </div>
-                    @endforeach
-                    <div>
-                        <a href="" class="allcategoryShow">
-                            <b>All Categories</b>
-                        </a>
-                    </div>
-                </section>
-            </div>
-=======
   <div class="bannerSet bannerSetcategory">
     <div class="wrapper">
       <h4>Welcome to XXX, where expertise meet opportunity.</h4>
@@ -110,122 +76,23 @@
                         <p>Lead a team to manage application enhancements, break-fixes, standard applications changes
                             and
                             maintenance activities.</p>
-                            <div class="categoryTabs">
-                <div class="wrapper">
-                    <div class="categoryTabsInner">
-                        <a href="" class="activeCategory">Business Offerings</a>
-                        <a href="">Consulting Services</a>
-                        <a href="">Events/Conferences</a>
-                        <a href="">Health Authority Sites</a>
-                        <a href="">Jobs</a>
-                    </div>
-                </div>
-            </div>
-            
+                        <b>Active 7 days ago</b>
+                       
                     </div>
                 </div>
   <div class="pharmCategory categoriesAll catgorySetsAll">
     <div class="wrapper">
       <div class="pharmCategoryInner">
         <section class="categorySet">
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/2.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/3.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/2.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
-          <div>
-            <a href="{{ route('subcategory') }}">
-              <span><img src="assets/images/categoriesIcon/1.png" alt="categoriy" /></span>
-              <h4>Consulting Services</h4>
-              <b>Learn More</b>
-            </a>
-          </div>
+        @foreach($data['categories'] as $category)
+                    <div>
+                        <a href="{{ route('subcategory',$category->id) }}">
+                            <span><img src="{{ url('storage/'.$category->image) }}" alt="categoriy" /></span>
+                            <h4>{{ $category->title}}</h4>
+                            <b>Learn More</b>
+                        </a>
+                    </div>
+                    @endforeach
           <div>
             <a href="" class="allcategoryShow">
               <b>View All</b>
@@ -626,58 +493,11 @@
               <img src="{{asset('/assets/images/logos/3.png') }}" alt="logo" />
             </div>
           </section>
->>>>>>> b8ff62040923b17edf92fd96be5033a24124e529
         </div>
+      </div>
     </div>
-<<<<<<< HEAD
-
-    <div class="pharmCategory pharmPartnersLogo">
-        <div class="wrapper">
-            <div class="headerTitle">
-                <h3>FEATURED</h3>
-                <p>Explore features of pharma</p>
-            </div>
-            <div class="pharmCategoryInner">
-                <div class="innerDifferentSection">
-                    <h4>Upcoming Events</h4>
-                    <section class="center slider">
-                        <div>
-                            <img src="{{asset('/assets/images/logos/1.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/2.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/3.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/1.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/3.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/2.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/1.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/2.png') }}" alt="logo" />
-                        </div>
-                        <div>
-                            <img src="{{asset('/assets/images/logos/3.png') }}" alt="logo" />
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-=======
   </div>
 </div>
 
 
->>>>>>> b8ff62040923b17edf92fd96be5033a24124e529
 @endsection
