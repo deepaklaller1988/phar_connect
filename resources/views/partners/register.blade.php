d8c7695d891787e32f91df678804c78212918084
Pawan Chandel <pawan.chandel.contriverz.com>
2024-04-11 17:49:51 +0530

    registration page error

--- resources/views/partners/register.blade.php
+++ resources/views/partners/register.blade.php
@@ -33,7 +33,7 @@
                                     class="col-md-4 col-form-label text-md-end">{{ __('Company Email') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="text"
+                                    <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}" required autocomplete="email">
 
