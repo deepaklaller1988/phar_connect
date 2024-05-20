ea645136c71c46e1aeb48818678b6168c8c6cd6c
Pawan Chandel <pawan.chandel.contriverz.com>
2024-05-20 10:03:20 +0530

    Health authority page change

--- resources/views/health-authority.blade.php
+++ resources/views/health-authority.blade.php
@@ -9,24 +9,14 @@
     </div>
     <div class="wrapper">
         <div class="fullHealthDetails">
-            <section class="healthAreas">
-                <ul>
-                    @foreach($data as $dt)
-                    <li><a href="#tab{{ $dt['id'] }}">{{ $dt['name'] }}</a></li>
-                    @endforeach
-                </ul>
-            </section>
             <section class="healthLinksRedirect">
-                @foreach($data as $key => $dt)
-                <div class="width100Set" id="tab{{ $dt['id'] }}">
-                    <h6>{{ $dt['name'] }}</h6>
+                <div class="width100Set" id="tab">
                     <ul>
-                        @foreach($dt['posts'] as $skey =>  $child)
+                        @foreach($posts as  $child)
                         <li><a href="{{ $child['company_website'] }}" target="_blank">{{ $child['title'] }}</a></li>
                         @endforeach
                         <ul>
                 </div>
-                @endforeach
             </section>
         </div>
     </div>
